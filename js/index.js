25f16908b59d1ba1f10be1a8fbd8e63429911fb1
SunriseVastu <128222589+SunriseVastu@users.noreply.github.com>
2023-03-18 19:47:57 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -28,3 +28,28 @@ $(function() {
         document.documentElement.scrollTop = 0;
     }
 });
+
+
+
+(function() {
+    emailjs.init('3lZCskaWhfmuBqy4v');
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        $('#loader').show();
+        this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm('contact_service', 'template_8ihd63f', this)
+            .then(function() {
+                console.log('SUCCESS!');
+                $("#contact-form")[0].reset();
+                $("#contact-form").addClass("d-none");
+                $("#success-msg").removeClass("d-none");
+            }, function(error) {
+                console.log('FAILED...', error);
+                alert("Some error occured. Please re-sumbit the form.");
+            });
+        $('#loader').hide();
+    });
+};
